5178da7076addda3bb4896b8790fe50bdc234c47
Sundar Urs <sundarkurs@gmail.com>
2020-05-20 21:28:50 +0530

    Added Auditing controller

---
+++ Auditing/Auditing.Api/Controllers/AuditingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Auditing.Mongo.Interfaces;
+using Auditing.Mongo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auditing.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuditingController : Controller
+    {
+        private readonly IMongoRepository<Person> _peopleRepository;
+
+        public AuditingController(IMongoRepository<Person> peopleRepository)
+        {
+            _peopleRepository = peopleRepository;
+        }
+
+        [HttpPost("registerPerson")]
+        public async Task AddPerson(string firstName, string lastName)
+        {
+            var person = new Person()
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            await _peopleRepository.InsertOneAsync(person);
+        }
+    }
+}
--- Auditing/Auditing.Api/Startup.cs
+++ Auditing/Auditing.Api/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Auditing.Mongo.Interfaces;
+using Auditing.Mongo.Repo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +27,8 @@ namespace Auditing.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped(typeof(IMongoRepository<>), typeof(MongoRepository<>));
+
             services.AddControllers();
         }
 
